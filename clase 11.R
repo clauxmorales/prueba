a6a9606ecf591a2945b3cdfe8e1e2d5f98fd3010
Claudia Morales <clausbiosb98@gmail.com>
2017-06-02 12:29:18 -0500

    modelo de esquema de 3er examen

---
+++ clase 11.R
@@ -0,0 +1,15 @@
+#Tercer examen de Programacion Cientifica
+#2017
+#Nombre:
+
+#ctrl +shift + R, creo secciones, mas ordenado y no mezclas script
+
+# pregunta1 ---------------------------------------------------------------
+
+# introduccion ------------------------------------------------------------
+
+# analisis ----------------------------------------------------------------
+
+# post-procesamiento ------------------------------------------------------
+
+
